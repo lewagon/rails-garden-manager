4f2d5bc2f5be8e54c002e11e0c2844014bb882fd
AnthoXLMA <contact@service-lite-plus.com>
2020-09-07 20:40:57 +0200

    development host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://app-14189bbb-06cd-4a62-8c14-18f945266a31.cleverapps.io/" }
+  config.action_mailer.default_url_options = { host: "https://www.lite-services.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.

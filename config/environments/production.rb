3702ff78b2b51170ba07338b5e8d5741363e2ae9
AnthoXLMA <contact@service-lite-plus.com>
2020-09-07 16:31:59 +0200

    Deployment Try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = { host: "https://app-14189bbb-06cd-4a62-8c14-18f945266a31.cleverapps.io/" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
